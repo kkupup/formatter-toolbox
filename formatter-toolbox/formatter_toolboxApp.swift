309c5d3a277e0bdd73e42ad83d7b5d02c250ffa8
geloo <geloo@mail.com>
2024-08-28 17:54:38 +0800

    Initial Commit

---
+++ formatter-toolbox/formatter_toolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  formatter_toolboxApp.swift
+//  formatter-toolbox
+//
+//  Created by wzh on 2024/8/28.
+//
+
+import SwiftUI
+
+@main
+struct formatter_toolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
